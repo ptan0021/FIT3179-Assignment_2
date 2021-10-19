2089cc830cdc1ac7787486f658a501886982a5e9
ptan0021 <ptan0021@student.monash.edu>
2021-10-19 10:46:13 +0800

    Update

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,14 +1,14 @@
-var vg_1 = "js/malaria.vg.json";
-vegaEmbed("#map1", vg_1).then(function(result) {
+var vg_1 = "js/treatment.vg.json";
+vegaEmbed("#treatment", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "js/tuberculosis.vg.json";
-vegaEmbed("#map2", vg_2).then(function(result) {
+var vg_2 = "js/incidence.vg.json";
+vegaEmbed("#incidence", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_3 = "js/hiv.vg.json";
-vegaEmbed("#map3", vg_3).then(function(result) {
+var vg_3 = "js/circle.vg.json";
+vegaEmbed("#circle", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
